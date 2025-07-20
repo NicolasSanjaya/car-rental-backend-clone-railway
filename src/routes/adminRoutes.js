cfbdc9061b39cc728528799054778b8643f37d1a
NicolasSanjaya <nicolassanjaya4823@gmail.com>
2025-07-20 16:18:39 +0700

    feat(middleware): adding middleware isAdmin to admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController.js");
-const { authenticateToken } = require("../middleware/authMiddleware.js");
+const {
+  authenticateToken,
+  isAdmin,
+} = require("../middleware/authMiddleware.js");
 const upload = require("../middleware/uploadMiddleware.js");
 
 router.post(
   "/cars",
   authenticateToken,
+  isAdmin,
   upload.single("image"),
   adminController.addCars
 );
 router.put(
   "/cars/:id",
   authenticateToken,
+  isAdmin,
   upload.single("image"),
   adminController.updateCars
 );
@@ -20,22 +25,30 @@ router.put(
 router.patch(
   "/cars/:id/availability",
   authenticateToken,
+  isAdmin,
   upload.single("image"),
   adminController.updateCarsAvailability
 );
-router.delete("/cars/:id", authenticateToken, adminController.deleteCars);
-router.get("/stats", authenticateToken, adminController.getStats);
+router.delete(
+  "/cars/:id",
+  authenticateToken,
+  isAdmin,
+  adminController.deleteCars
+);
+router.get("/stats", authenticateToken, isAdmin, adminController.getStats);
 
 // Bookings
 router.get("/bookings", authenticateToken, adminController.getAllBookings);
 router.put(
   "/bookings/:id/status",
   authenticateToken,
+  isAdmin,
   adminController.updateBookings
 );
 router.delete(
   "/bookings/:id",
   authenticateToken,
+  isAdmin,
   adminController.deleteBookings
 );
 
