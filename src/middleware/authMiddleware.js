b9ac01bb06304b48440268b75211cef12f3ca5d7
NicolasSanjaya <nicolassanjaya4823@gmail.com>
2025-07-20 20:34:58 +0700

    fixed

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,25 +3,41 @@ require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 exports.authenticateToken = (req, res, next) => {
-  // const authHeader = req.headers["authorization"];
-  // const token = authHeader && authHeader.split(" ")[1];
+  const authHeader = req.headers["authorization"];
+  const headerToken = authHeader && authHeader.split(" ")[1];
+
   const token = req.cookies?.token;
 
-  if (!token) {
+  console.log({ headerToken });
+  console.log({ token });
+
+  if (!token && !headerToken) {
     return res
       .status(401)
       .json({ success: false, message: "Access token is required" });
   }
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) {
-      return res
-        .status(403)
-        .json({ success: false, message: "Invalid or expired token" });
-    }
-    req.user = user; // Simpan payload token (misal: {id, email}) di req.user
-    next();
-  });
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ success: false, message: "Invalid or expired token" });
+      }
+      req.user = user; // Simpan payload token (misal: {id, email}) di req.user
+      next();
+    });
+  } else if (headerToken) {
+    jwt.verify(headerToken, JWT_SECRET, (err, user) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ success: false, message: "Invalid or expired token" });
+      }
+      req.user = user; // Simpan payload token (misal: {id, email}) di req.user
+      next();
+    });
+  }
 };
 
 // Middleware untuk cek role admin
